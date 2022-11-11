dbe0b35727e72d2a2beabb546b374046bb0cedc9
Monica Crowe <mcrowe9194@gmail.com>
2022-11-10 22:55:33 -0600

    Added more code

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //string
+            //int
+            //char
+            //bool
+            //double
+            //decimal
+
+            string catName = "Gypsy";
+            int catAge = 2;
+            char middleInitial = 'S';
+            bool isLazy = true;
+            double catWeight = 9.5;
+            decimal foodPrice = 8.50m;
+
+
+            Console.WriteLine($"My cat's name is {catName}, her middle initial is {middleInitial} for Sue! She is {catAge} years old and she weighs {catWeight} pounds. It's {isLazy}, she sleeps all day! Her bags of food are ${foodPrice}, she eats all the time so that must be why she's so lazy!");
         }
+
+        
     }
 }
